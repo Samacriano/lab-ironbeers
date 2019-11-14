47939ef0a2198bfcbd98e4621dbb1ac754454fd4
samacriano mendes <samacriano@gmail.com>
2019-11-14 08:31:46 +0000

    not done

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,11 +10,43 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+//register partial to use partials like navbar
+hbs.registerPartials(__dirname + "/views/partials");
+
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index'); 
 });
+
+app.get('/beer', (req, res, next) => {
+
+  punkAPI
+  .getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    next(error);
+  });
+
+   
+});
+
+app.get('/randomBeer', (req, res, next) => {
+
+  punkAPI
+  .getRandom()
+  .then(beer => {
+    res.render('randomBeer', {beer});
+  })
+  .catch(error => {
+    next(error);
+  });
+   
+});
+
+
 
 
 
